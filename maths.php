fe27400e9036b120227849fc8de44695a9126b32
lidiettes <hello@lidiettes.com>
2022-09-02 10:09:20 +0200

    maths

---
+++ maths.php
@@ -0,0 +1,33 @@
+<?php
+
+// Define a variable whose value is the result of the function that returns an absolute value.
+$a = -2;
+echo abs($a);
+echo '<br>';
+echo '<br>';
+
+// Define a variable whose value is the result of the function that returns a rounded value to the next highest integer.
+$b = 2.6;
+echo ceil($b);
+echo '<br>';
+echo '<br>';
+// Define a variable whose value is the result of the function that returns the highest value of a series of values ​​that are received by parameter.
+$c = array(1,2,3,4,5);
+echo max($c);
+echo '<br>';
+echo '<br>';
+
+// Define a variable whose value is the result of the function that returns the lowest value of a series of values ​​that are received by parameter.
+$d = array(1,2,3,4,5);
+echo min($d);
+echo '<br>';
+echo '<br>';
+
+// Define a variable whose value is the result of the function that returns a random number
+
+echo rand();
+echo '<br>';
+echo '<br>';
+
+
+?>
---
+++ types.php
@@ -0,0 +1,44 @@
+<?php
+
+// boolean
+$boolean = true;
+var_dump($boolean);
+echo '<br>';
+echo '<br>';
+
+// integer
+$integer = 1;
+var_dump($integer);
+echo '<br>';
+echo '<br>';
+
+// float
+$float = 1.5;
+var_dump ($float);
+echo '<br>';
+echo '<br>';
+
+// string
+$string = 'Hello';
+var_dump($string);
+echo '<br>';
+echo '<br>';
+
+// array
+$array= array(1,2,3,4);
+var_dump($array);
+
+// object
+class Obj {
+    public $key=0;
+}
+$obj= new Obj;
+echo '<br>';
+echo '<br>';
+
+// NULL
+$bar = null;
+var_dump($bar);
+echo '<br>';
+echo '<br>';
+?>
